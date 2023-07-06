e83007fdfe033a52433f7a9b71ff1209d787ed3c
ASUS <ASUS@LAPTOP-VKJ537Q5>
2023-07-06 22:00:40 +0700

    lan 4

---
+++ src/doanjavacoban1/bai1/Ham1.java
@@ -0,0 +1,44 @@
+package doanjavacoban1.bai1;
+
+import java.util.Scanner;
+
+public class Ham1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("a = ");
+		double a = sc.nextDouble();
+		System.out.print("b = ");
+		double b = sc.nextDouble();
+		System.out.print("c = ");
+		double c = sc.nextDouble();
+		System.out.print("x = ");
+		double x = sc.nextDouble();
+		double f = a * x * x + b * x + c;
+		System.out.println("f(x) = " + f);
+
+		System.out.print("a = ");
+		a = sc.nextDouble();
+		System.out.print("b = ");
+		b = sc.nextDouble();
+		System.out.print("c = ");
+		c = sc.nextDouble();
+		System.out.print("x = ");
+		x = sc.nextDouble();
+		f = a * x * x + b * x + c;
+		System.out.println("f(x) = " + f);
+		
+		System.out.print("a = ");
+		a = sc.nextDouble();
+		System.out.print("b = ");
+		b = sc.nextDouble();
+		System.out.print("c = ");
+		c = sc.nextDouble();
+		System.out.print("x = ");
+		x = sc.nextDouble();
+		f = a * x * x + b * x + c;
+		System.out.println("f(x) = " + f);
+	}
+
+}
